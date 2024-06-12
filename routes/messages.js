fd7b72f9268cc4bbfc4b6fdb2907389446c39f51
Andrew <andrew.harnedy@gmail.com>
2024-06-11 20:47:15 -0400

    messa

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,10 @@
+const Router = require("express").Router;
+const router = new Router();
+
+const Message = require("../models/message");
+const {ensureLoggedIn} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +18,22 @@
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
+
+        if (msg.to_user.username !== username && msg.from_user.username !== username) {
+            throw new ExpressError("Cannot read this message", 401);
+        }
+
+        return res.json({message: msg});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
 
 /** POST / - post message.
  *
@@ -27,4 +50,43 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let msg = await Message.create({
+            from_username: req.user.username,
+            to_username: req.body.to_username,
+            body: req.body.body
+        });
 
+        return res.json({message: msg});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
+
+/** To mark message as read:
+ * => {message: {id, read_at}}
+ * This will make sure that only the intended recipient can mark as read.
+ */
+
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
+
+        if (msg.to_user.username !== username) {
+            throw new ExpressError("Cannot set this message to read", 401);
+        }
+        let message = await Message.markRead(req.params.id);
+
+        return res.json({message});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
